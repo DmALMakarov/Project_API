819393083b43fe837a98288cfba2757e00f648ff
B!aB!aMakar <106482607+DmALMakarov@users.noreply.github.com>
2022-12-14 00:21:12 +0300

    first commit

---
+++ requests.py
@@ -0,0 +1,45 @@
+import pip._vendor.requests
+from pprint import pprint
+
+
+def get_token():
+    with open('token.txt') as file:
+        return file.readline()
+
+
+class YandexDisk:
+    files_url = "https://cloud-api.yandex.net/v1/disk/resources/files"
+    upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+
+    def __init__(self, token: str):
+        self.token = token
+
+    @property
+    def headers(self):
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"OAuth {self.token}"
+        }
+
+    def get_upload_link(self, file_path):
+        params = {"path": file_path, "overwrite": "true"}
+        response = pip._vendor.requests.get(self.upload_url, params=params, headers=self.headers)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, file_path):
+        href = self.get_upload_link(file_path).get("href")
+        if not href:
+            return
+
+        with open(file_path, "rb") as file:
+            response = pip._vendor.requests.put(href, data=file)
+            if response.status_code == 201:
+                print("Файл загружен")
+                return True
+            print("Файл не загружен потому что", response.status_code)
+            return False
+
+
+yandex_client = YandexDisk(get_token())
+yandex_client.upload("file.txt")
